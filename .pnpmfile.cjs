5f669f57783a43a337408e49a179ab285b59924f
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-01 23:49:01 +0200

    ci: remove Cypress dependency patch

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -2,20 +2,15 @@ module.exports = {
   hooks: {
     readPackage: (package) => {
       switch (package.name) {
-        // case '@samverschueren/stream-to-observable':
-        //   package.dependencies['any-observable'] = '~0.3.0';
-
-        //   break;
         case 'any-observable':
           package.dependencies['rxjs'] = '~6.6.0';
 
           break;
-        case 'cypress':
-          // case 'listr':
-          package.dependencies['any-observable'] = '~0.3.0';
-          package.dependencies['rxjs'] = '~6.6.0';
+        // case 'cypress':
+        //   package.dependencies['any-observable'] = '~0.3.0';
+        //   package.dependencies['rxjs'] = '~6.6.0';
 
-          break;
+        //   break;
       }
 
       return package;
