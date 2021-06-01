76422f3e6de05fec27a8fe2a989ebec3d34fddb1
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-01 23:42:04 +0200

    ci: add back rxjs dependency patch

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -4,21 +4,16 @@ module.exports = {
       switch (package.name) {
         case '@samverschueren/stream-to-observable':
           package.dependencies['any-observable'] = '~0.3.0';
-          // package.dependencies['rxjs'] = '~6.6.0';
 
           break;
         case 'any-observable':
-          // package.dependencies['@samverschueren/stream-to-observable'] =
-          //   '~0.3.1';
-          // package.dependencies['rxjs'] = '~6.6.0';
+          package.dependencies['rxjs'] = '~6.6.0';
 
           break;
         case 'cypress':
         case 'listr':
-          // package.dependencies['@samverschueren/stream-to-observable'] =
-          //   '~0.3.1';
           package.dependencies['any-observable'] = '~0.3.0';
-          // package.dependencies['rxjs'] = '~6.6.0';
+          package.dependencies['rxjs'] = '~6.6.0';
 
           break;
       }
