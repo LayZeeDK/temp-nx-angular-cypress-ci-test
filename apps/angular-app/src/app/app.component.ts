f9f88e5525b40ba4028a91f08cdd4d1623dbed91
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-01 21:26:41 +0200

    initial commit

---
+++ apps/angular-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'temp-nx-angular-cypress-ci-test-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'angular-app';
+}
