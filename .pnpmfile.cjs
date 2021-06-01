d9f52349d311cb4062257bfee74fd8d4ac49981e
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-01 23:44:23 +0200

    ci: try removing most dependency patches

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -2,16 +2,16 @@ module.exports = {
   hooks: {
     readPackage: (package) => {
       switch (package.name) {
-        case '@samverschueren/stream-to-observable':
-          package.dependencies['any-observable'] = '~0.3.0';
+        // case '@samverschueren/stream-to-observable':
+        //   package.dependencies['any-observable'] = '~0.3.0';
 
-          break;
-        case 'any-observable':
-          package.dependencies['rxjs'] = '~6.6.0';
+        //   break;
+        // case 'any-observable':
+        //   package.dependencies['rxjs'] = '~6.6.0';
 
-          break;
+        //   break;
         case 'cypress':
-        case 'listr':
+          // case 'listr':
           package.dependencies['any-observable'] = '~0.3.0';
           package.dependencies['rxjs'] = '~6.6.0';
 
