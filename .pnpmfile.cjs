43e2f5830bf9baa8eaaaed8518d7256931552b4b
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-01 23:55:16 +0200

    ci: turn dependency patch into configuration

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,17 +1,12 @@
 module.exports = {
   hooks: {
     readPackage: (package) => {
-      switch (package.name) {
-        case 'any-observable':
-          package.dependencies['rxjs'] = '~6.6.0';
+      // switch (package.name) {
+      //   case 'any-observable':
+      //     package.dependencies['rxjs'] = '~6.6.0';
 
-          break;
-        // case 'cypress':
-        //   package.dependencies['any-observable'] = '~0.3.0';
-        //   package.dependencies['rxjs'] = '~6.6.0';
-
-        //   break;
-      }
+      //     break;
+      // }
 
       return package;
     },
