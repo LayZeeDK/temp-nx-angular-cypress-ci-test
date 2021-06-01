d506db71881bbad21d4f9ee54c1218c98c9be343
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-02 00:32:53 +0200

    ci: patch @nrwl/cypress to use Webpack 5

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -2,6 +2,10 @@ module.exports = {
   hooks: {
     readPackage: (package) => {
       switch (package.name) {
+        case '@nrwl/cypress':
+          package.dependencies['@cypress/webpack-preprocessor'] = '^5.9.0';
+
+          break;
         case 'any-observable':
           package.dependencies.rxjs = package.devDependencies.rxjs;
 
