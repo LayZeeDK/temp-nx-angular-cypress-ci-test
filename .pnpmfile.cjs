fd5c1e67205ecd45afcd842719f3617245b76cb0
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-06-02 21:42:32 +0200

    ci: hoist webpack

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -2,15 +2,15 @@ module.exports = {
   hooks: {
     readPackage: (package) => {
       switch (package.name) {
-        // case '@cypress/webpack-preprocessor':
-        //   package.dependencies.webpack = '^5.30.0';
-        //   package.peerDependencies.webpack = '^5.30.0';
+        case '@cypress/webpack-preprocessor':
+          package.dependencies.webpack = '^5.30.0';
+          package.peerDependencies.webpack = '^5.30.0';
 
-        //   break;
-        // case '@nrwl/cypress':
-        //   package.dependencies['@cypress/webpack-preprocessor'] = '^5.9.0';
+          break;
+        case '@nrwl/cypress':
+          package.dependencies['@cypress/webpack-preprocessor'] = '^5.9.0';
 
-        //   break;
+          break;
         case 'any-observable':
           package.dependencies.rxjs = package.devDependencies.rxjs;
 
